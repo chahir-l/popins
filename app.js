fae20ee598cb220ee803e7a41eba8d3e4b1d4a0e
Artois <artoislabs@gmail.com>
2020-09-02 19:38:51 +0200

    version 2 popin

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 class Popin
 {
-    constructor(popinClass, eltOpen, eltClose)
+
+    width = 300;
+    height = 300;
+    popinClass = 'popin';
+    eltOpenClass = 'open-popin';
+    eltCloseClass = "close-popin";
+    txtClose = "close";
+
+    constructor(infosObject)
     {
-         
-        this.popin = document.querySelector(popinClass);
-        this.eltOpen = document.querySelector(eltOpen);
-        this.eltClose = document.querySelector(eltClose);
-   
+        if(typeof infosObject !== "undefined")
+         {
+
+            this.popinClass = typeof infosObject.popinClass !== "undefined" ? infosObject.popinClass :  this.popinClass;
+            this.eltOpen = typeof infosObject.eltOpen !== "undefined" ? infosObject.popinClass :  this.eltOpenClass;
+            this.eltClose = typeof infosObject.eltClose !== "undefined" ? infosObject.popinClass  : this.eltCloseClass;
+         }
+        console.log(this.eltCloseClass);
+        this.createPopin();
+        this.popin = document.querySelector( "." + this.popinClass);
+        this.eltOpen = document.querySelector( "." + this.eltOpenClass);
+        this.eltClose = document.querySelector("." + this.eltCloseClass);
+        console.log(this.eltOpen)
         this.eltOpen.addEventListener('click', this.open.bind(this));
         this.eltClose.addEventListener('click', this.close.bind(this));
     }
@@ -19,5 +35,20 @@ class Popin
         this.popin.style.display = 'block';
     }
 
-    n
+    createPopin()
+    {
+        let popin = document.createElement('div');
+        let linkClose = document.createElement('a');
+        linkClose.setAttribute("class", this.eltCloseClass);
+        linkClose.setAttribute("href", "#");
+        linkClose.innerText = "close";
+
+        popin.appendChild(linkClose);
+
+        popin.style.width = this.width;
+        popin.style.height = this.height;
+        popin.classList.add(this.popinClass)
+        console.log(popin)
+        document.body.appendChild(popin)
+    }
 }
